c34b846bf8b21ddf7462806d2f6233bd5833da20
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2016-12-27 11:46:52 +0000

    Created Dish model class

---
+++ app/src/main/java/model/Dish.java
@@ -0,0 +1,63 @@
+package model;
+
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+
+public class Dish {
+    private String name;
+    private String fotoUrl;
+    private List<String> allergens;
+    private BigDecimal price;
+    private Currency currency;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFotoUrl() {
+        return fotoUrl;
+    }
+
+    public void setFotoUrl(String fotoUrl) {
+        this.fotoUrl = fotoUrl;
+    }
+
+    public List<String> getAllergens() {
+        return allergens;
+    }
+
+    public void setAllergens(List<String> allergens) {
+        this.allergens = allergens;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
