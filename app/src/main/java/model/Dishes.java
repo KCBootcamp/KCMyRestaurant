bc7413c50d14171e74673f7f3f85106a92bf7c1f
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2016-12-27 12:18:20 +0000

    Created Dishes Class

---
+++ app/src/main/java/model/Dishes.java
@@ -0,0 +1,29 @@
+package model;
+
+
+import java.util.List;
+
+public class Dishes {
+
+    private List<Dish> dishes;
+
+    public Dishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
+    public void setDishes(List<Dish> dishes) {
+        this.dishes = dishes;
+    }
+
+    public Dish getDish(int position){
+        return dishes.get(position);
+    }
+
+    public int getCount() {
+        return dishes.size();
+    }
+}
