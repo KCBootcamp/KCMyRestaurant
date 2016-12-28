19ca442157c0f401cdcac1f7ae3e257c4f587a28
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2016-12-28 10:18:27 +0000

    Refactor

--- app/src/main/java/es/bhavishchandnani/myrestaurant/activities/MainActivity.java
+++ app/src/main/java/es/bhavishchandnani/myrestaurant/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package es.bhavishchandnani.myrestaurant;
+package es.bhavishchandnani.myrestaurant.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import es.bhavishchandnani.myrestaurant.R;
 
 public class MainActivity extends AppCompatActivity {
 
