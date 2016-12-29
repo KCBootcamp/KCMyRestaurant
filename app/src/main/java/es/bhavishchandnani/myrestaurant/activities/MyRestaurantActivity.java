d414076909e746aa77863472dd0b08b6b5a86e42
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2016-12-29 13:39:28 +0000

    Added / Modified Resources: icons

---
+++ app/src/main/java/es/bhavishchandnani/myrestaurant/activities/MyRestaurantActivity.java
@@ -0,0 +1,15 @@
+package es.bhavishchandnani.myrestaurant.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import es.bhavishchandnani.myrestaurant.R;
+
+public class MyRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_restaurant);
+    }
+}
