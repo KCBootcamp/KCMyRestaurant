9ba0c09fea9b66f6d8021fbb461492c9602f85c2
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2016-12-31 09:16:27 +0000

    Created Constant Static Class

---
+++ app/src/main/java/es/bhavishchandnani/myrestaurant/utils/Constants.java
@@ -0,0 +1,12 @@
+package es.bhavishchandnani.myrestaurant.utils;
+
+/**
+ * Created by bhavishchandnani on 12/12/16.
+ */
+
+public class Constants {
+    // Messages (strings) only for development
+    public static String appName = "es.bhavishchandnani.myrestaurant";
+
+
+}
