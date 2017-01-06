a14f04071edbdc2321f5e4b36fd643be268e6421
Bhavish Chandnani Chandnani <bhavish.chandnani@gmail.com>
2017-01-06 10:55:38 +0100

    Modified toolbar custom view

--- app/src/main/java/es/bhavishchandnani/myrestaurant/views/CustomViews.java
+++ app/src/main/java/es/bhavishchandnani/myrestaurant/views/CustomViews.java
@@ -10,34 +10,36 @@ import android.widget.TextView;
 import es.bhavishchandnani.myrestaurant.R;
 
 public class CustomViews {
-    public static void setCustomToolbarView(AppCompatActivity activity, String title, int leftBtnImageId, int rightBtnImageId, final ToolbarClickInterface listener) {
+    public static void setCustomToolbarView(AppCompatActivity activity, String title, int leftBtnImageId, int rightBtnImageId, final ToolbarInterface toolbarInterface) {
         View view = activity.findViewById(android.R.id.content);
 
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
-        toolbar.setPadding(0,0,0,0);
+        if (view.findViewById(R.id.toolbar) != null) {
+            Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+            toolbar.setPadding(0, 0, 0, 0);
 
-        TextView textView = (TextView) view.findViewById(R.id.toolbar_title);
-        textView.setText(title);
+            TextView textView = (TextView) view.findViewById(R.id.toolbar_title);
+            textView.setText(title);
 
-        ImageView leftImageButton = (ImageView) view.findViewById(R.id.toolbar_left_btn);
-        leftImageButton.setImageResource(leftBtnImageId);
-        leftImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                listener.onLeftButtonClicked();
-            }
-        });
+            ImageView leftImageButton = (ImageView) view.findViewById(R.id.toolbar_left_btn);
+            leftImageButton.setImageResource(leftBtnImageId);
+            leftImageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    toolbarInterface.onLeftButtonClicked();
+                }
+            });
 
-        ImageView rightImageButton = (ImageView) view.findViewById(R.id.toolbar_right_btn);
-        rightImageButton.setImageResource(rightBtnImageId);
-        rightImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                listener.onRightButtonClicked();
-            }
-        });
+            ImageView rightImageButton = (ImageView) view.findViewById(R.id.toolbar_right_btn);
+            rightImageButton.setImageResource(rightBtnImageId);
+            rightImageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    toolbarInterface.onRightButtonClicked();
+                }
+            });
 
-        activity.setSupportActionBar(toolbar);
-        activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+            activity.setSupportActionBar(toolbar);
+            activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
     }
 }
